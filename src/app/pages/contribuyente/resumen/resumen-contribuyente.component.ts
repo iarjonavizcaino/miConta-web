24341692f305cdafb74ca188d25649422a867e63
sagonzalez <sagonzalez@ittepic.edu.mx>
2018-01-30 14:44:01 -0600

    Feat: change products table on bill, add bimester and year in top right of page, add on egresos status 'cobrado' and 'pendiente'

--- src/app/pages/contribuyente/resumen/resumen-contribuyente.component.ts
+++ src/app/pages/contribuyente/resumen/resumen-contribuyente.component.ts
@@ -20,9 +20,10 @@ export class ResumenContribuyenteComponent implements OnInit, OnDestroy {
   ];
   headersEgresos: Array<RtHeader> = [
     { name: 'Fecha', prop: 'date', default: 'No date', moment: true },
+    { name: 'Estado', prop: 'status', default: 'Pendiente', chip: true },
     { name: 'Proveedor', prop: 'customer.name', default: 'No customer' },
-    { name: 'RFC', prop: 'customer.rfc', default: 'XXXX-XXX-XXXX' },
-    { name: 'Tipo', prop: 'type', default: '', align: 'center', chip: true },
+    // { name: 'RFC', prop: 'customer.rfc', default: 'XXXX-XXX-XXXX' },
+    { name: 'Tipo de factura', prop: 'type', default: '', align: 'center', chip: true },
     { name: 'Total', prop: 'total', default: '$ 0.00', align: 'right', accounting: true }
   ];
 
@@ -45,6 +46,9 @@ export class ResumenContribuyenteComponent implements OnInit, OnDestroy {
   years = [];
   bimesters = [];
 
+  currentBimester = 'ENE-FEB 2018';
+  selectedYear = '';
+  selectedBimester = '';
   constructor(private router: Router, private dialogCtrl: MatDialog, private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -326,6 +330,7 @@ export class ResumenContribuyenteComponent implements OnInit, OnDestroy {
   loadEgresosData() {
     this.dataEgresos = [
       {
+        status: 'Cobrado',
         type: 'MANUAL',
         active: false,
         provider: true,
@@ -371,6 +376,7 @@ export class ResumenContribuyenteComponent implements OnInit, OnDestroy {
         ]
       },
       {
+        status: 'Pendiente',
         type: 'AUTOMATICA',
         active: false,
         provider: true,
@@ -416,6 +422,7 @@ export class ResumenContribuyenteComponent implements OnInit, OnDestroy {
         ]
       },
       {
+        status: 'Cobrado',
         type: 'XML',
         active: false,
         provider: true,
@@ -487,5 +494,9 @@ export class ResumenContribuyenteComponent implements OnInit, OnDestroy {
         name: 'NOV-DIC'
       }
     ];
+  }
+
+  getBimesterInfo(ev: any) {
+    this.currentBimester = this.selectedBimester + ' ' + this.selectedYear;
   }
 }// class
