7a884c2a155c5a7c95a7d459dc1507ad528589e7
Jessica Pardo <jelipardope@ittepic.edu.mx>
2018-02-22 21:30:15 -0700

    feat: genrate bill by xml

--- src/app/pages/_catalog/upload-xml/upload-xml.component.ts
+++ src/app/pages/_catalog/upload-xml/upload-xml.component.ts
@@ -47,8 +47,76 @@ export class UploadXmlComponent implements OnInit {
     this.xml = true;
     const xml_str = ev[1].files.file; // XML text
     // parse to json
-    const jsonBill = xml2json.xml2json(xml_str, { compact: true, spaces: 4 }); // convert to json
-    this.files.push(JSON.parse(jsonBill));
+    let jsonBill: any = xml2json.xml2json(xml_str, { compact: true, spaces: 4 }); // convert to json
+    jsonBill = jsonBill.replace(/cfdi:/g, '');
+    jsonBill = jsonBill.replace(/sferp:/g, '');
+    jsonBill = JSON.parse(jsonBill);
+
+    console.log(jsonBill);
+    const payment = jsonBill.Comprobante._attributes.FormaPago;
+    const type = jsonBill.Comprobante._attributes.TipoDeComprobante;
+    const newBill = {
+      type: type === 'I' ? 'INGRESOS' : 'EGRESOS',
+      createdDate: jsonBill.Comprobante._attributes.Fecha,
+      cobrada_pagada: jsonBill.Comprobante._attributes.MetodoPago === 'PUE' ? true : false,
+      cobrada_pagadaDate: jsonBill.Comprobante._attributes.MetodoPago === 'PUE' ? jsonBill.Comprobante._attributes.Fecha : null,
+      deducible: '',
+      payMethod: {
+        key: payment,
+        // tslint:disable-next-line:max-line-length
+        method: payment === '01' ? 'Efectivo' : payment === '02' ? 'Cheque nominativo' : payment === '03' ? 'Transferencia electrónica de fondos' : 'Por definir',
+      },
+      general_public: '',
+      captureMode: 'XML',
+      taza: 0,
+      taxes: 0,
+      subtotal: jsonBill.Comprobante._attributes.SubTotal,
+      total: jsonBill.Comprobante._attributes.Total,
+      customer_provider: {
+        name: type === 'I' ? jsonBill.Comprobante.Receptor._attributes.Nombre : jsonBill.Comprobante.Emisor._attributes.Nombre,
+        rfc: type === 'I' ? jsonBill.Comprobante.Receptor._attributes.Rfc : jsonBill.Comprobante.Emisor._attributes.Rfc,
+        address: {
+          street: '',
+          number: '',
+          neighborhood: '',
+          zipcode: '',
+          city: '',
+          municipality: '',
+          state: '',
+        }
+      },
+      products: []
+    };
+
+    let product;
+
+    if (jsonBill.Comprobante.Conceptos.Concepto.length) {
+      jsonBill.Comprobante.Conceptos.Concepto.forEach(concept => {
+        product = {
+          code: concept._attributes.ClaveProdServ.toString().substr(0, 4),
+          concept: concept._attributes.Descripcion,
+          product: concept._attributes.Descripcion,
+          quantity: concept._attributes.Cantidad,
+          price: concept._attributes.ValorUnitario,
+          amount: concept._attributes.Importe
+        };
+        newBill.products.push(product);
+      });
+    } else {
+      product = {
+        code: jsonBill.Comprobante.Conceptos.Concepto._attributes.ClaveProdServ.toString().substr(0, 4),
+        concept: jsonBill.Comprobante.Conceptos.Concepto._attributes.Descripcion,
+        product: jsonBill.Comprobante.Conceptos.Concepto._attributes.Descripcion,
+        quantity: jsonBill.Comprobante.Conceptos.Concepto._attributes.Cantidad,
+        price: jsonBill.Comprobante.Conceptos.Concepto._attributes.ValorUnitario,
+        amount: jsonBill.Comprobante.Conceptos.Concepto._attributes.Importe
+      };
+      newBill.products.push(product);
+    }
+
+
+    console.log(newBill);
+    this.files.push(newBill);
   }
 
   onUploadError(ev: any) {
