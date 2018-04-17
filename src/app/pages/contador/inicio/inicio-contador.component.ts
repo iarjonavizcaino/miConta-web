fa1a88492fbf8294e29f3b1ed7a19fbff9ee2bbf
Jessica Pardo <jelipardope@ittepic.edu.mx>
2018-04-16 22:59:11 -0600

    fix: update taxpayer

--- src/app/pages/contador/inicio/inicio-contador.component.ts
+++ src/app/pages/contador/inicio/inicio-contador.component.ts
@@ -110,6 +110,7 @@ export class InicioContadorComponent implements OnInit, OnDestroy {
     const dialogRef = this.taxpayerModal(this.selectedTaxpayer, true, 'Detalle contribuyente');
     dialogRef.afterClosed().subscribe((taxpayer) => {
       if (!taxpayer) { return; }
+      console.log(taxpayer);
       this.taxpayerProv.update(taxpayer).subscribe(data => {
         taxpayer = data.taxpayer;
         this.action.next({ name: RtActionName.UPDATE, itemId: taxpayer._id, newItem: taxpayer });
